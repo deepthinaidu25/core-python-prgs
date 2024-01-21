b5a00c637aaa8a62776cf83192d57ea859068ffe
deepthi <127131195+deepthinaidu25@users.noreply.github.com>
2024-01-21 11:40:23 +0530

    Update 102.py

--- 102.py
+++ 102.py
@@ -3,5 +3,5 @@ for i in range(1, 11):
     ele = int(input(f"Enter element {i}: "))
     list1.append(ele)
 
-# Find and print the largest element in the list
+
 print("Largest element is:", max(list1))
