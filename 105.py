b019399e213ffe1f804167bc5aa7a0c3273faf52
deepthi <127131195+deepthinaidu25@users.noreply.github.com>
2024-01-21 12:03:12 +0530

    Update 105.py

--- 105.py
+++ 105.py
@@ -1,7 +1,7 @@
 #105.	Write a Python program to find smallest in the given N numbers
-list1=[]
-N=int(input("enter the 10 numbers:"))
-for N in range(1,N+1):
+N=int(input("enter the  N numbers:"))
+for N in range(1,11):
+    
     ele=int(input("enter elements:"))
     list1.append(ele)
-print("the smallest of the 10 numbers is:",min(list1))
+print("the smallest of the N numbers is:",min(list1))
